a67f2166c9b06dd1aa43dea4a32aa95349d6401f
shenzhenbin <shenzb@aispreadtech.com>
2019-02-22 18:18:24 +0800

    update

--- manager/src/main/java/com/aispread/manager/leave/mapper/StatutoryHolidayMapper.java
+++
@@ -1,8 +0,0 @@
-package com.aispread.manager.leave.mapper;
-
-import com.aispread.manager.leave.entity.StatutoryHolidayEntity;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface StatutoryHolidayMapper extends BaseMapper<StatutoryHolidayEntity> {
-
-}
--- manager/src/main/java/com/aispread/manager/leave/service/HolidayRecordService.java
+++
@@ -1,8 +0,0 @@
-package com.aispread.manager.leave.service;
-
-import com.aispread.manager.leave.entity.HolidayRecordEntity;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface HolidayRecordService extends IService<HolidayRecordEntity> {
-    String getWorkdayTimeInMillisExcWeekendHolidays(HolidayRecordEntity holidayRecordEntity);
-}
